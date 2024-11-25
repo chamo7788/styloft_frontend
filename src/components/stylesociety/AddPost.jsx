86f3593ad8244d799f862da3811029d1efdbffde
Chamodya Ganegamage <139947960+chamo7788@users.noreply.github.com>
2024-11-25 23:12:49 +0530

    hello

---
+++ src/components/stylesociety/AddPost.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function AddPost() {
+    return (
+        <div>
+            <h1>Add Post</h1>
+            <p>Welcome to the Add Post page!</p>
+        </div>
+    );
+}
---
+++ src/components/stylesociety/TrendingCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function TrendingCard() {
+    return (
+        <div>
+            <h1>TrendingCard</h1>
+            <p>Welcome to the TrendingCard page!</p>
+        </div>
+    );
+}
