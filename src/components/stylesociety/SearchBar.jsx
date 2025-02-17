86076b932c0763b2f2b5a8e457cb8c1462b600c7
Hasindi0518 <hasindienasha1@gmail.com>
2025-02-17 22:35:47 +0530

    Search_Bar

--- src/components/stylesociety/SearchBar.jsx
+++ src/components/stylesociety/SearchBar.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
+import "../../assets/css/StyleSociety/searchBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 export function SearchBar() {
     return (
-        <div>
-            <h1>Post</h1>
-            <p>Welcome to the Post page!</p>
+        <div className="search-container">
+            <div className="search-bar">
+                <input type="text" placeholder="Search Designers" />
+                <button>
+                    <FontAwesomeIcon icon={faSearch} />
+                </button>
+            </div>
+            <h1 className="top-projects">Top projects you may like</h1>
+            <p className="subtitle">These projects are highly rated by other clients</p>
         </div>
     );
 }
+export default SearchBar;
--- src/components/stylesociety/TrendingCard.jsx
+++ src/components/stylesociety/TrendingCard.jsx
@@ -3,8 +3,7 @@ import React from "react";
 export function TrendingCard() {
     return (
         <div>
-            <h1>TrendingCard</h1>
-            <p>Welcome to the TrendingCard page!</p>
+           
         </div>
     );
 }
