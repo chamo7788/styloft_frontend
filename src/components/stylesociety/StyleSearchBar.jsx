59837bd3a458ebe4bde511562f78aa1ed05c4752
Hasindi0518 <hasindienasha1@gmail.com>
2025-03-07 23:40:39 +0530

    Add SearchBar

--- src/components/stylesociety/StyleSearchBar.jsx
+++ src/components/stylesociety/StyleSearchBar.jsx
@@ -1,21 +1,30 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Search } from "lucide-react";
 import "../../assets/css/StyleSociety/styleSearchBar.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export function StyleSearchBar() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
     return (
         <div className="stylesearch-container">
             <div className="stylesearch-bar">
-                <input type="text" placeholder="Search Designers" />
-                <button>
-                    <FontAwesomeIcon icon={faSearch} />
-                </button>
+                <input 
+                    type="text" 
+                    placeholder="Search Designers"  
+                    className="Style" 
+                    value={searchQuery} 
+                    onChange={handleSearchChange}
+                />
+                <Search className="sty" />
             </div>
             <h1 className="styletop-projects">Top projects you may like</h1>
             <p className="stylesubtitle">These projects are highly rated by other clients</p>
         </div>
     );
 }
+
 export default StyleSearchBar;
