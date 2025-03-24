872a3b9e64a1d3690a94dee3a4976c5470860fbc
Chamodya Ganegamage <139947960+chamo7788@users.noreply.github.com>
2025-03-24 12:01:27 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Add this to handle peer dependency issues
+  build: {
+    outDir: 'build', // Change output directory from 'dist' to 'build'
+  },
   optimizeDeps: {
     include: ['@react-three/fiber', '@react-three/postprocessing'],
   },
